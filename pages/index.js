698f84c1cc63c228a7819dddc0bcfa8da3ae649a
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-10-08 20:54:00 -0700

    Pushing next sanity

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,40 @@ import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
 import CardListItem from 'components/CardListItem';
 import CardItem from 'components/CardItem';
+import { getAllBlogs } from 'lib/api';
 
-export default function Home() {
+export default function Home({ blogs }) {
 
 return(
   <PageLayout>
     <AuthorIntro />
     <hr/>
     <Row className="mb-5">
-      <Col md="10">
+      {/* <Col md="10">
         <CardListItem />
-      </Col>
+      </Col> */}
 
-      <Col md="4">
-        <CardItem />
-      </Col>
+    {blogs.map(blog => (  
+      <Col md="4" key={blog.slug}>
+        <CardItem 
+        title={blog.title}
+        subtitle={blog.subtitle}
+        slug={blog.slug} />
+      </Col>))}
+    
     </Row>
   </PageLayout>
   )
+}
+
+// This function is called during the build (build time)
+// Provides props to your page
+// It will create static page
+export async function getStaticProps() {
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs 
+    }
+  }
 }
