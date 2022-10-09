eeaecd1c64d8d38e3d059cdd380731a006254165
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-10-09 13:31:56 -0700

    font and minor styling updates

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,21 +1,29 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { Container } from 'react-bootstrap';
-import NavBar from "./NavBar"
+import NavBar from "./NavBar";
+import Head from 'next/head';
+
 
 function PageLayout({ children, className }) {
 return (
-  <Container>
-    <NavBar />
-    <div className={`page-wrapper className`}>
-      { children }
-    </div>
-    <footer className="page-footer">
+  <>
+    <Head>
+      <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap" rel="stylesheet" />
+    </Head>
+    <Container>
+      <NavBar />
+      <div className={`page-wrapper className`}>
+        {children}
+      </div>
+      <footer className="page-footer">
         <div>
           <a href="#">courses</a>{' | '}
           <a href="#">github</a>{' | '}
           <a href="#">facebook</a>
         </div>
       </footer>
-  </Container>  
+    </Container>
+  </>  
 )
 }
 
