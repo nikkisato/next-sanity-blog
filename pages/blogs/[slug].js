324c30e59dd68e4a3ebcfc7576d79f4daede8444
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-10-09 19:50:13 -0700

    getStaticPaths function

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -3,17 +3,37 @@ import { useRouter } from "next/router"
 import { getBlogBySlug } from "lib/api"
 
 const BlogDetail = ({ blog }) => { 
-
+    console.log("Displaying Page")
     return (
     <PageLayout>
         <h1>Hello Detail Page - {blog.slug}</h1>
     </PageLayout>
 )}
 
-export async function getServerSideProps({ params }) {
+export async function getStaticProps({ params }) {
+    console.log("fetching blog by", params.slug)
+
     const blog = await getBlogBySlug(params.slug);
     return {
         props: {blog}
+    }
+}
+
+export async function getStaticPaths() {
+    console.log("Getting paths for every page")
+    return {
+        paths:[
+            {
+                params: {slug: "my-second-blog"}
+            },
+            {
+                params: {slug: "my-first-blog"}
+            },
+            {
+                params: {slug: "my-third-blog"}
+            },
+        ],
+        fallback: false
     }
 }
 
