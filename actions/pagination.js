b2a4da673e74d2b099d324a8ca38d5e1e5e4e636
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2022-10-11 15:45:32 -0700

    passing Offset

--- actions/pagination.js
+++ actions/pagination.js
@@ -8,7 +8,7 @@ import CardListItem from 'components/CardListItem';
  export const useGetBlogsPages = ({ blogs: initialData, filter }) => {
     return useSWRPages('index-page',
         ({ offset, withSWR }) => { 
-            const { data: blogs } = withSWR(useGetBlogs(initialData));
+            const { data: blogs } = withSWR(useGetBlogs({offset}));
 
             if (!blogs) {return "Loading..."}
             return (blogs.map(blog => (  
@@ -46,7 +46,10 @@ import CardListItem from 'components/CardListItem';
         // SWR: data you will get from "withSWR" function
         // index: number of current page
         (SWR, index) => {
-            return 0;
+            if (SWR.data && SWR.data.length === 0) {
+                return null;
+            }
+            return (index + 1 )* 3;
         },
         [filter]
     )
