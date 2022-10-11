1851c5b182573c7faf3875a15241e926e67bdce0
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-10-10 20:16:36 -0700

    added font awesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
+import { library, config } from '@fortawesome/fontawesome-svg-core';
+import { faBorderAll, faList } from '@fortawesome/free-solid-svg-icons';
+
+config.autoAddCss = false;
+library.add(faList, faBorderAll);
+
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "highlight.js/styles/dracula.css";
 import 'styles/index.scss';
