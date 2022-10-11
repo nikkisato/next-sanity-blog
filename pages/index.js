8d43267b363e4a7e4d796b99eb066dc9126ecee0
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2022-10-11 15:30:04 -0700

    Button to load

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Row, Col} from 'react-bootstrap';
+import { Row, Col, Button} from 'react-bootstrap';
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
 import CardListItem from 'components/CardListItem';
@@ -18,7 +18,16 @@ export default function Home({blogs}) {
     }
   })
 
-  const { pages, isLoadingMore, isReachingEnd, loadMore } = useGetBlogsPages({ blogs, filter });
+// loadMore: to load more data from
+// isLoadingMore: is true whenever we are making request to fetch data
+// isReachingEnd: is true when we loaded all of the data, data is empty (empty array)
+
+const { 
+    pages, 
+    isLoadingMore, 
+    isReachingEnd, 
+    loadMore } = useGetBlogsPages({ blogs, filter });
+
 console.log(blogs)
 return(
   <PageLayout>
@@ -33,6 +42,15 @@ return(
     <Row className="mb-5">
       { pages }
     </Row>
+    <div style={{textAlign: 'center'}}>
+      <Button 
+        size="lg"
+        variant="outline-secondary"
+        onClick={loadMore}
+        disabled={ isReachingEnd || isLoadingMore }>
+        {isLoadingMore ? "..." : isReachingEnd ? "No More Blogs" : "More Blogs" }
+      </Button>
+    </div>
   </PageLayout>
   )
 }
