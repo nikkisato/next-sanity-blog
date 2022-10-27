5ae2edd84707c25bd5926c4ef790c1312a4a0a9c
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2022-10-26 19:31:11 -0700

    pagination improvement

--- actions/pagination.js
+++ actions/pagination.js
@@ -1,85 +1,82 @@
-/* eslint-disable react-hooks/rules-of-hooks */
+
+
 import { useSWRPages } from 'swr';
 import { useGetBlogs } from 'actions';
 import { Col } from 'react-bootstrap';
 import CardItem from 'components/CardItem';
-import CardListItem from 'components/CardListItem';
-import { useEffect } from 'react';
 import CardItemBlank from 'components/CardItemBlank';
+import CardListItem from 'components/CardListItem';
 import CardListItemBlank from 'components/CardListItemBlank';
-import moment from "moment"
+import moment from 'moment';
 
- export const useGetBlogsPages = ({ blogs, filter }) => {
+export const BlogList = ({blogs, filter}) => {
+  return blogs
+  .map(blog =>
+  filter.view.list ?
+    <Col key={`${blog.slug}-list`} md="9">
+      <CardListItem
+        author={blog.author}
+        title={blog.title}
+        subtitle={blog.subtitle}
+        date={moment(blog.date).format('LL')}
+        link={{
+          href: '/blogs/[slug]',
+          as: `/blogs/${blog.slug}`
+        }}
+      />
+    </Col>
+    :
+    <Col key={blog.slug} lg="4" md="6">
+      <CardItem
+        author={blog.author}
+        title={blog.title}
+        subtitle={blog.subtitle}
+        date={moment(blog.date).format('LL')}
+        image={blog.coverImage}
+        link={{
+          href: '/blogs/[slug]',
+          as: `/blogs/${blog.slug}`
+        }}
+      />
+    </Col>
+  )
+}
 
-    useEffect(() => {
-        window.__pagination__init = true;
-    },[]);
+export const useGetBlogsPages = ({filter, blogs}) => {
 
+  return useSWRPages(
+    'index-page',
+    ({offset, withSWR}) => {
+      const { data: paginatedBlogs, error } =  withSWR(useGetBlogs({offset, filter}));
 
-    return useSWRPages('index-page',
-        ({ offset, withSWR }) => { 
-            let initialData = !offset && blogs;
+      if (!offset && !paginatedBlogs && !error) {
+        return <BlogList blogs={blogs} filter={filter}/>
+      }
 
-            if (typeof window !== "undefined" && window.__pagination__init ) {
-                initialData = null;
-            }
-            const { data: paginatedBlogs } = withSWR(useGetBlogs({offset, filter}, initialData));
+      if (!paginatedBlogs) {
+        return Array(3)
+          .fill(0)
+          .map((_, i) =>
+            filter.view.list ?
+              <Col key={i} md="9">
+                <CardListItemBlank />
+              </Col>
+              :
+              <Col key={`${i}-item`} lg="4" md="6">
+                <CardItemBlank />
+              </Col>
+          )
+      }
 
-            if (!paginatedBlogs) {
-                return Array(3)
-                    .fill(0)
-                    .map((_, i) => {
-                        filter.view.list ?
-                        <Col key={`${i}-list`} md="9">
-                            <CardListItemBlank />
-                        </Col>
-                        :
-                        <Col key={`${i}-item`} md="4">
-                            <CardItemBlank />
-                        </Col>
-                    });   
-            }
-
-            return (paginatedBlogs.map(blog => (  
-                filter.view.list ?
-                <Col md="9" key={`${blog.slug}-list`}>
-                <CardListItem
-                    author={blog.author}
-                    title={blog.title}
-                    subtitle={blog.subtitle}
-                    date={moment(blog.date).format("LLL")}
-                    link={{
-                    href: '/blogs/[slug]',
-                    as: `/blogs/${blog.slug}`
-                    }}
-                /> 
-                </Col>
-                :
-                <Col md="4" key={blog.slug}>
-                    <CardItem 
-                    author={blog.author}
-                    title={blog.title}
-                    subtitle={blog.subtitle}
-                    date={blog.date}
-                    image={blog?.coverImage}
-                    link={{
-                        href: '/blogs/[slug]',
-                        as: `/blogs/${blog.slug}`
-                    }}
-                    />
-                </Col> 
-                ))
-            )
-        }, 
-        // here you will compute offset that will get passed into previous callback functions
-        // SWR: data you will get from "withSWR" function
-        // index: number of current page
-        (SWR, index) => {
-            if (SWR.data && SWR.data.length === 0) {
-                return null;
-            }
-            return (index + 1 )* 6;
-        },
-        [filter]
-    )
- };
+      return <BlogList blogs={paginatedBlogs} filter={filter} />
+    },
+    // here you will compute offset that will get passed into previous callback function with 'withSWR'
+    // SWR: data you will get from 'withSWR' function
+    // index: number of current page
+    (SWR, index) => {
+      if (SWR.data && SWR.data.length === 0) { return null; }
+      return (index + 1) * 6;
+    },
+    [filter]
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,57 @@
-import { useEffect, useState } from "react";
-import { Row, Button} from 'react-bootstrap';
+
+import { useState } from 'react';
+
+import { Row, Button } from 'react-bootstrap';
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
 import FilteringMenu from 'components/FilteringMenu';
 import PreviewAlert from 'components/PreviewAlert';
 
-import { getAllBlogs } from 'lib/api';
-import { useGetBlogsPages } from "actions/pagination"
-
+import { useGetBlogsPages } from 'actions/pagination';
+import { getPaginatedBlogs } from 'lib/api';
 
 export default function Home({blogs, preview}) {
   const [filter, setFilter] = useState({
     view: { list: 0 },
     date: { asc: 0 }
-  })
+  });
 
-// loadMore: to load more data from
-// isLoadingMore: is true whenever we are making request to fetch data
-// isReachingEnd: is true when we loaded all of the data, data is empty (empty array)
+  const {
+    pages,
+    isLoadingMore,
+    isReachingEnd,
+    loadMore
+  } = useGetBlogsPages({filter, blogs});
 
-const { 
-    pages, 
-    isLoadingMore, 
-    isReachingEnd, 
-    loadMore } = useGetBlogsPages({ blogs, filter });
-
-return(
-  <PageLayout>
-    { preview && <PreviewAlert /> }
-    <AuthorIntro />
-    <FilteringMenu
-    filter={filter}
-      onChange={(option, value) => {
-        setFilter({...filter, [option]: value})
-      }} 
-    />
-    <hr/>
-    <Row className="mb-5">
-      { pages }
-    </Row>
-    <div style={{textAlign: 'center'}}>
-      <Button 
-        size="lg"
-        variant="outline-secondary"
-        onClick={loadMore}
-        disabled={ isReachingEnd || isLoadingMore }>
-        {isLoadingMore ? "..." : isReachingEnd ? "No More Blogs" : "More Blogs" }
-      </Button>
-    </div>
-  </PageLayout>
+  return (
+    <PageLayout>
+      { preview && <PreviewAlert /> }
+      <AuthorIntro />
+      <FilteringMenu
+        filter={filter}
+        onChange={(option, value) =>
+          setFilter({...filter, [option]: value})
+        }
+      />
+      <hr/>
+      <Row className="mb-5">
+        {pages}
+      </Row>
+      <div style={{textAlign: 'center'}}>
+        <Button
+          onClick={loadMore}
+          disabled={isReachingEnd || isLoadingMore}
+          size="lg"
+          variant="outline-secondary">
+          {isLoadingMore ? '...' : isReachingEnd ? 'No more blogs' : 'More Blogs'}
+        </Button>
+      </div>
+    </PageLayout>
   )
 }
 
 export async function getStaticProps({preview = false}) {
-  const blogs = await getAllBlogs({offset: 0, date: "desc"});
+  const blogs = await getPaginatedBlogs({offset: 0, date: 'desc'});
   return {
     props: {
       blogs, preview
